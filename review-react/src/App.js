97487f886843db893307a21cb8926abbba0b4222
wldnr1208@naver.com <wldnr1208@naver.com>
2023-05-16 23:46:20 +0900

    [Feat] router 설정 복습

--- review-react/src/App.js
+++ review-react/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import VideoDetail from "./pages/VideoDetail";
+
+/* element은 React Router에서 사용되는 속성으로, 해당 경로(path)에 대한 컴포넌트를 지정하는 역할 */
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/videos",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/:videoId",
+        element: <VideoDetail />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
